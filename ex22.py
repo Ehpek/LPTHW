776ac8ac84b412991fb2a54e81067213a7fb9dac
Ian White <Ian@Ians-MacBook-Pro.local>
2019-06-02 12:39:08 -0500

    Checking in next batch of exercises

---
+++ ex22.py
@@ -0,0 +1,15 @@
+# self review
+print - evaluates and writes the resulting object to standard output
+# - an octothorpe or hash, used for writing comments
++ plus adds things together
+- minus subtracts things together
+/ slash divids things
+* asterisk multiplies things
+% percent gives remainder in decimal of an expression
+< less-than evalutes in a true/false method
+> greater-than evalutes in a true/false method
+<= less-than-equal evalutes in a true/false method
+>= greater-than-equal evalutes in a true/false method
+= equals surrounded by spaces, when not in a print string, used to set a variable
+== evaluates if two things are the same evalutes
+print(f"") an f-string allows inclusion of expressions within a string
